1f30c8cccaaa25c716b4c42f3825774eacd35426
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-25 12:06:35 +0900

    Step2 (#2)
    * 모달 기능 추가
    
    * 모달 애니메이션 추가
    
    * will-change 추가
    
    * 불필요한 변경사항 제거

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (() => {
   const LIST_ITEM_COUNT = 100;
+  const MODAL_Z_INDEX = '1000';
 
   /**
    * @param {number} index
@@ -17,10 +18,64 @@
     return [index - 1, index + 1];
   }
 
+  document.body.style.overflowX = 'hidden';
+
+  const modalContent = document.getElementById('modal-content');
+  modalContent.style.display = 'flex';
+  modalContent.style.justifyContent = 'center';
+  modalContent.style.alignItems = 'center';
+  modalContent.style.backgroundColor = 'rgb(255, 255, 255)';
+  modalContent.style.border = '1px solid black';
+  modalContent.style.borderRadius = '4px';
+  modalContent.style.width = '70vw';
+  modalContent.style.height = '70vh';
+  modalContent.style.fontSize = '10vw';
+  modalContent.style.willChange = 'transform';
+  modalContent.addEventListener('click', (event) => { event.stopPropagation(); });
+
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+  modal.style.position = 'fixed';
+  modal.style.top = '0';
+  modal.style.left = '0';
+  modal.style.zIndex = MODAL_Z_INDEX;
+  modal.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+  modal.style.width = '100%';
+  modal.style.height = '100%';
+  modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modal.style.justifyContent = '';
+    modal.style.alignItems = '';
+
+    modalContent.innerHTML = '';
+  });
+
+  /**
+   * @param {number} index
+   */
+  function showModal(index) {
+    modal.style.display = 'flex';
+    modal.style.justifyContent = 'center';
+    modal.style.alignItems = 'center';
+
+    modalContent.animate([
+      { transform: 'scale(0.99)' },
+      { transform: 'scale(1.01)' },
+      { transform: 'scale(1)' }
+    ], {
+      duration: 200,
+      iterations: 1,
+      easing: 'ease-out',
+    })
+
+    modalContent.innerHTML = `${index}`;
+  }
+
   const listItems = Array.from({length: LIST_ITEM_COUNT}).map(() => {
     const listItem = document.createElement('li');
     listItem.style.willChange = 'transform';
-    listItem.style.transition = 'transform .2s ease-out';
+    listItem.style.transition = 'transform 0.2s ease-out';
+    listItem.style.cursor = 'pointer';
 
     return listItem;
   });
@@ -44,7 +99,8 @@
         array[sibiling].style.transform = '';
       })
     })
+    listItem.addEventListener('click', () => { showModal(index); });
 
     listParent.appendChild(listItem);
-  })
+  });
 })();
