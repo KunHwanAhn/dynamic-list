bab40cf87d319cb2c4214e077741db9b7d96cf01
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-25 11:03:41 +0900

    Step1 (#1)
    * hover 이벤트 추가와 위치 변경
    
    * 형제 엘리먼트에도 transform 적용
    
    * 변수 위치 조정

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log('hello world');
+(() => {
+  const LIST_ITEM_COUNT = 100;
+
+  /**
+   * @param {number} index
+   * @param {number} maxIndex
+   */
+  function getSibilings(index, maxIndex = LIST_ITEM_COUNT - 1) {
+    if (index === 0) {
+      return [index + 1];
+    }
+
+    if (index === maxIndex) {
+      return [index - 1];
+    }
+
+    return [index - 1, index + 1];
+  }
+
+  const listItems = Array.from({length: LIST_ITEM_COUNT}).map(() => {
+    const listItem = document.createElement('li');
+    listItem.style.willChange = 'transform';
+    listItem.style.transition = 'transform .2s ease-out';
+
+    return listItem;
+  });
+
+  const listParent = document.getElementById('dynamic-list');
+
+  listItems.forEach((listItem, index, array) => {
+    const sibilings = getSibilings(index);
+
+    listItem.addEventListener('mouseenter', () => {
+      listItem.style.transform = 'translateX(40px)';
+
+      sibilings.forEach((sibiling) => {
+        array[sibiling].style.transform = 'translateX(20px)';
+      })
+    });
+    listItem.addEventListener('mouseleave', () => {
+      listItem.style.transform = '';
+
+      sibilings.forEach((sibiling) => {
+        array[sibiling].style.transform = '';
+      })
+    })
+
+    listParent.appendChild(listItem);
+  })
+})();
