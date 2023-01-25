bed22871c9924f6176c73fb2c4c4c887d1f8f00a
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-25 11:00:45 +0900

    형제 엘리먼트에도 transform 적용

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
 (() => {
   const LIST_ITEM_COUNT = 100;
 
+  /**
+   * @param {number} index
+   */
+  function getSibilings(index, maxIndex = LIST_ITEM_COUNT - 1) {
+    if (index === 0) {
+      return [index + 1];
+    }
+
+    if (index === maxIndex) {
+      return [index - 1];
+    }
+
+    return [index - 1, index + 1];
+  }
+
   const listParent = document.getElementById('dynamic-list');
 
-  Array.from({length: LIST_ITEM_COUNT}).forEach((_, index) => {
+  const listItems = Array.from({length: LIST_ITEM_COUNT}).map(() => {
     const listItem = document.createElement('li');
     listItem.style.willChange = 'transform';
     listItem.style.transition = 'transform .2s ease-out';
 
-    listItem.addEventListener('mouseenter', (event) => {
+    return listItem;
+  });
+
+  listItems.forEach((listItem, index, array) => {
+    const sibilings = getSibilings(index);
+
+    listItem.addEventListener('mouseenter', () => {
       listItem.style.transform = 'translateX(40px)';
+
+      sibilings.forEach((sibiling) => {
+        array[sibiling].style.transform = 'translateX(20px)';
+      })
     });
     listItem.addEventListener('mouseleave', () => {
       listItem.style.transform = '';
+
+      sibilings.forEach((sibiling) => {
+        array[sibiling].style.transform = '';
+      })
     })
 
     listParent.appendChild(listItem);
   })
-
-
-  console.log(listParent);
 })();
