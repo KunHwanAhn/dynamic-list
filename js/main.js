3d5b15b0b65822fb2caa0d0a06e136c9ceb6ef8d
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-25 10:31:52 +0900

    hover 이벤트 추가와 위치 변경

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+(() => {
+  const LIST_ITEM_COUNT = 100;
+
+  const listParent = document.getElementById('dynamic-list');
+
+  Array.from({length: LIST_ITEM_COUNT}).forEach((_, index) => {
+    const listItem = document.createElement('li');
+    listItem.style.willChange = 'transform';
+    listItem.style.transition = 'transform .2s ease-out';
+
+    listItem.addEventListener('mouseenter', (event) => {
+      listItem.style.transform = 'translateX(40px)';
+    });
+    listItem.addEventListener('mouseleave', () => {
+      listItem.style.transform = '';
+    })
+
+    listParent.appendChild(listItem);
+  })
+
+
+  console.log(listParent);
+})();
