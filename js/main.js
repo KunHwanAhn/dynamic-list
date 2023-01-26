0980d513dc4f9692bb8599d3e1fc3a09ab723278
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-26 20:23:36 +0900

    모달 방식에서 Popout 방식으로 변경

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (() => {
   const LIST_ITEM_COUNT = 100;
-  const MODAL_Z_INDEX = '1000';
+  const POPOUT_Z_INDEX = 1000;
 
   /**
    * @param {number} index
@@ -18,81 +18,107 @@
     return [index - 1, index + 1];
   }
 
-  document.body.style.overflowX = 'hidden';
-
-  const modalContent = document.getElementById('modal-content');
-  modalContent.style.display = 'flex';
-  modalContent.style.justifyContent = 'center';
-  modalContent.style.alignItems = 'center';
-  modalContent.style.backgroundColor = 'rgb(255, 255, 255)';
-  modalContent.style.border = '1px solid black';
-  modalContent.style.borderRadius = '4px';
-  modalContent.style.width = '70vw';
-  modalContent.style.height = '70vh';
-  modalContent.style.fontSize = '10vw';
-  modalContent.style.willChange = 'transform';
-  modalContent.addEventListener('click', (event) => { event.stopPropagation(); });
-
-  const modal = document.getElementById('modal');
-  modal.style.display = 'none';
-  modal.style.position = 'fixed';
-  modal.style.top = '0';
-  modal.style.left = '0';
-  modal.style.zIndex = MODAL_Z_INDEX;
-  modal.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-  modal.style.width = '100vw';
-  modal.style.height = '100vh';
-  modal.addEventListener('click', () => { hideModal(); });
-
-  function hideModal() {
-    document.body.style.overflowY = 'auto';
-    modal.style.display = 'none';
-    modal.style.justifyContent = '';
-    modal.style.alignItems = '';
-
-    modalContent.innerHTML = '';
-  }
+  let popoutIndex = -1;
 
   /**
    * @param {number} index
    */
-  function showModal(index) {
-    document.body.style.overflowY = 'hidden';
-    modal.style.display = 'flex';
-    modal.style.justifyContent = 'center';
-    modal.style.alignItems = 'center';
-
-    modalContent.animate(
-      [
-        { transform: 'scale(0.8)', opacity: 0 },
-        { transform: 'scale(1.01)', opacity: 1, offset: 0.8 },
-        { transform: 'scale(1)' },
-      ],
-      {
-        duration: 200,
-        iterations: 1,
-        easing: 'ease-out',
-      }
-    );
-
-    // NOTE: 리스트 아이템의 숫자와 맞추기 위해서 1을 더해줌
-    modalContent.innerHTML = `${index + 1}`;
+  function setPopoutIndex(index) {
+    popoutIndex = index;
   }
+
+  /**
+   * @returns popoutIndex
+   */
+  function getPopoutIndex() {
+    return popoutIndex;
+  }
+
+  /**
+   * @param {number} index
+   * @param {HTMLDivElement} bg
+   */
+  function showPopout(index, bg) {
+    document.body.style.overflowY = 'hidden';
+    bg.style.display = 'block';
+
+    const target = listItems[index];
+    target.style.justifyContent = 'center';
+    target.style.fontSize = '4vw';
+    target.style.transition = '';
+    target.style.position = 'fixed';
+    target.style.insetBlockStart = '50%';
+    target.style.insetInlineStart = '50%';
+    target.style.transform = 'translate(-50%, -50%)';
+    target.style.width = '70vw';
+    target.style.height = '70vh';
+    target.style.cursor = '';
+    target.style.zIndex = `${POPOUT_Z_INDEX}`;
+  }
+
+  /**
+   * @param {number} index
+   * @param {HTMLDivElement} bg
+   */
+  function hidePopout(index, bg) {
+    document.body.style.overflowY = 'auto';
+    setPopoutIndex(-1);
+    resetListItem(listItems[index]);
+
+    const sibilings = getSibilings(index);
+    sibilings.forEach((sibiling) => {
+      resetListItem(listItems[sibiling]);
+    });
+
+    bg.style.display = 'none';
+  }
+
+  /**
+   * @param {HTMLLIElement} target
+   */
+  function resetListItem(target) {
+    target.style.listStyle = 'none';
+    target.style.marginBlockStart = '0.5rem';
+    target.style.padding = '1rem';
+    target.style.border = '1px solid black';
+    target.style.position = 'relative';
+    target.style.background = 'white';
+    target.style.willChange = 'transform';
+    target.style.transition = 'transform 0.2s ease-out';
+    target.style.transform = '';
+    target.style.cursor = 'pointer';
+    target.style.display = 'flex';
+    target.style.alignItems = 'center';
+    target.style.justifyContent = '';
+    target.style.fontSize = '';
+    target.style.insetBlockStart = '';
+    target.style.insetInlineStart = '';
+    target.style.width = '';
+    target.style.height = '';
+    target.style.zIndex = '';
+  }
+
+  document.body.style.overflowX = 'hidden';
 
   const listItems = Array.from({length: LIST_ITEM_COUNT}).map((_, index) => {
     const listItem = document.createElement('li');
-    listItem.style.listStyle = 'none';
-    listItem.style.marginBlockStart = '0.5rem';
-    listItem.style.padding = '1rem';
-    listItem.style.border = '1px solid black';
-    listItem.style.background = 'white';
-    listItem.style.willChange = 'transform';
-    listItem.style.transition = 'transform 0.2s ease-out';
-    listItem.style.cursor = 'pointer';
     listItem.innerHTML = `${index + 1}`;
+    resetListItem(listItem);
 
     return listItem;
   });
+
+  const popoutBackground = document.getElementById('popout-bg');
+  popoutBackground.style.display = 'none';
+  popoutBackground.style.position = 'fixed';
+  popoutBackground.style.insetBlockStart = '0';
+  popoutBackground.style.insetInlineStart = '0';
+  popoutBackground.style.width = '100%';
+  popoutBackground.style.height = '100%';
+  popoutBackground.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+  popoutBackground.style.cursor = 'pointer';
+  popoutBackground.style.zIndex = `${POPOUT_Z_INDEX - 1}`;
+  popoutBackground.addEventListener('click', () => { hidePopout(getPopoutIndex(), popoutBackground); });
 
   const listParent = document.getElementById('dynamic-list');
   listParent.style.paddingInlineStart = '0';
@@ -100,21 +126,35 @@
   listItems.forEach((listItem, index, array) => {
     const sibilings = getSibilings(index);
 
+    // NOTE: 리스트 아이템에 hover 했을 때
     listItem.addEventListener('mouseenter', () => {
+      if (index === getPopoutIndex()) {
+        return;
+      }
+
       listItem.style.transform = 'translateX(40px)';
 
       sibilings.forEach((sibiling) => {
         array[sibiling].style.transform = 'translateX(20px)';
       })
     });
-    listItem.addEventListener('mouseleave', () => {
-      listItem.style.transform = '';
 
-      sibilings.forEach((sibiling) => {
-        array[sibiling].style.transform = '';
-      })
-    })
-    listItem.addEventListener('click', () => { showModal(index); });
+    // NOTE: 리스트 아이템을 벗어났을 때
+    listItem.addEventListener('mouseleave', () => {
+      if (index === getPopoutIndex()) {
+        return;
+      }
+
+      resetListItem(listItem);
+
+      sibilings.forEach((sibiling) => { resetListItem(array[sibiling]); });
+    });
+
+    // NOTE: 리스트 아이템을 선택했을 때
+    listItem.addEventListener('click', () => {
+      setPopoutIndex(index);
+      showPopout(getPopoutIndex(), popoutBackground);
+    });
 
     listParent.appendChild(listItem);
   });
